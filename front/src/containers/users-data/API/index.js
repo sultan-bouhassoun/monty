510ecd8bbc807fb5b078351fae11b944a3a85916
Sauce <smb26@mail.aub.edu>
2019-10-09 21:47:06 +0300

    Finishing the UI. Implemented Addition of data and submission to server

--- front/src/containers/users-data/API/index.js
+++ front/src/containers/users-data/API/index.js
@@ -1,20 +1,21 @@
 // fetch requests to the server will be exported from this file
 const url = 'http://localhost:6060/'
 // The post call
-// export const postData = fetch(url+'', {
-//     method: 'POST', // *GET, POST, PUT, DELETE, etc.
-//     mode: 'cors', // no-cors, *cors, same-origin
-//     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//     credentials: 'same-origin', // include, *same-origin, omit
-//     headers: {
-//       'Content-Type': 'application/json'
-//       // 'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//     redirect: 'follow', // manual, *follow, error
-//     referrer: 'no-referrer', // no-referrer, *client
-//     body: JSON.stringify(data) // body data type must match "Content-Type" header
-//   });
-
+export const postData = (data) => { 
+  return fetch(url+'api/data', {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+}
   // The get call
   export const getData = fetch(url+'api/data', {
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
--- front/src/containers/users-data/users-data.js
+++ front/src/containers/users-data/users-data.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
-import SimpleTable from '../../components/Table'
-import { getData, postData } from './API/index'
+import React, {Component, Fragment} from 'react';
+import SimpleTable from '../../components/Table';
+import { getData, postData } from './API/index';
+import './users-data.scss'
 class UsersDataTable extends Component {
     constructor(props){
         super(props)
@@ -13,11 +14,11 @@ class UsersDataTable extends Component {
                 "Protein (g)"
             ],
             rowsData: [
-                this.createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-                this.createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-                this.createData('Eclair', 262, 16.0, 24, 6.0),
-                this.createData('Cupcake', 305, 3.7, 67, 4.3),
-                this.createData('Gingerbread', 356, 16.0, 49, 3.9),
+                this.createData('Frozen yoghurt local', 159, 6.0, 24, 4.0),
+                this.createData('Ice cream sandwich local', 237, 9.0, 37, 4.3),
+                this.createData('Eclair local', 262, 16.0, 24, 6.0),
+                this.createData('Cupcake local', 305, 3.7, 67, 4.3),
+                this.createData('Gingerbread local', 356, 16.0, 49, 3.9),
             ],
             editRowID: ''
         }
@@ -26,6 +27,8 @@ class UsersDataTable extends Component {
         this.deleteData = this.deleteData.bind(this)
         this.triggerEdit = this.triggerEdit.bind(this)
         this.stopEdit = this.stopEdit.bind(this)
+        this.sendToServer = this.sendToServer.bind(this)
+        this.submitFormHandler = this.submitFormHandler.bind(this)
     }
 
     createData = (name, calories, fat, carbs, protein) => {
@@ -41,9 +44,7 @@ class UsersDataTable extends Component {
     }
 
     editData(e, fieldName, rowID){
-        console.log(e, fieldName, rowID);
         const {value} = e.target;
-        console.log(value);
         this.setState(state =>({
            rowsData: state.rowsData.map(row => row.name==rowID ? ({...row, [fieldName]:value}) : row) 
         }))
@@ -54,26 +55,72 @@ class UsersDataTable extends Component {
             rowsData: state.rowsData.filter(row => row.name!==name)
         }))
     }
+    sendToServer(){
+        postData(this.state.rowsData);
+    }
+
+    submitFormHandler(event){
+        event.preventDefault();
+        const data = new FormData(event.target);
+        // I am treating the name as if it is an ID
+        // you can't create New Row without supplying a name
+        // there could be other forms of validation that can be made. 
+        // like for example checking if the fields other than Name field are numbers or not
+        if(data.get('name')){
+            const newRow = this.createData(data.get('name'),data.get('cal'),data.get('fat'),data.get('carb'),data.get('protein'))
+            this.setState(state => ({
+                rowsData: [...state.rowsData,newRow]
+            }))
+        }
+
+    }
 
     componentDidMount(){
         getData.then(res => {
-            res.json().then(body => console.log('sadad',body))
+            res.json().then(body => {
+                let serverData = body.map(row => this.createData(row.name, row.calories, row.fat, row.carbs, row.protein))
+                this.setState(state => ({
+                    rowsData: serverData
+                }))
+            })
         })
+
     }
 
     render(){
-        console.log(this.state.rowsData)
         return(
-            <SimpleTable
-                rows={this.state.rowsData}
-                headers={this.state.tableHeaders}
-                editRowID={this.state.editRowID}
-                triggerEdit={this.triggerEdit}
-                stopEdit={this.stopEdit}
-                edit={this.editData}
-                delete={this.deleteData}
-            >
-            </SimpleTable>
+            <Fragment>
+                {/* change to Material UI later https://material-ui.com/api/form-control/ */}
+                <form  onSubmit={this.submitFormHandler}>
+                    <label>
+                        <input type="text" name="name" placeholder={"Dessert Name:"}/>
+                    </label><br></br>
+                    <label>
+                        <input type="text" name="cal" placeholder={"Calories:"}/>
+                    </label><br></br>
+                    <label>
+                        <input type="text" name="fat" placeholder={"Fat:"}/>
+                    </label><br></br>
+                    <label>
+                        <input type="text" name="carb" placeholder={"Carbs:"}/>
+                    </label><br></br>
+                    <label>
+                        <input type="text" name="protein" placeholder={"Protein:"}/>
+                    </label><br></br>
+                    <input className={"inputButton"} type="submit" value="ADD"/>
+                </form>
+                <SimpleTable
+                    rows={this.state.rowsData}
+                    headers={this.state.tableHeaders}
+                    editRowID={this.state.editRowID}
+                    triggerEdit={this.triggerEdit}
+                    stopEdit={this.stopEdit}
+                    edit={this.editData}
+                    delete={this.deleteData}
+                >
+                </SimpleTable>
+                <button onClick={this.sendToServer}>Submit</button>
+            </Fragment>
         )
     }
 }
